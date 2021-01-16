444b352bbe05038353fa4052fd306ef6e756259b
Aile Castberg <ailecastberg@outlook.com>
2021-01-16 20:18:37 +0100

    Add project files.

---
+++ LetsSettleITRockPaperScissors/Base/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace LetsSettleITRockPaperScissors
+{
+    public class BaseViewModel : INotifyPropertyChanged, ICommand
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler CanExecuteChanged;
+
+        protected static NavigationService Navigate { get; } = (Application.Current.MainWindow as MainWindow).Main.NavigationService;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
